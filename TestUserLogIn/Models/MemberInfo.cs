8ff1e1214a84702d866e62d9fc80c23eb3b501da
adnix4 <anixdorf@student.cvtc.edu>
2025-10-29 19:37:28 -0500

    Validation

--- TestUserLogIn/Models/MemberInfo.cs
+++ TestUserLogIn/Models/MemberInfo.cs
@@ -24,14 +24,25 @@ namespace TestUserLogIn.Models
 
         public string? Zip { get; set; }
 
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        [Display(Name = "Cell Phone Number")]
         public string? CellPhoneNumber { get; set; }
 
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
         public string? HomePhoneNumber { get; set; }
 
         public string? WorkPhoneNumber { get; set; }
 
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string? Email { get; set; }
 
+        [EmailAddress(ErrorMessage = "Please enter a valid preferred email address")]
+        [Display(Name = "Preferred Contact Email")]
+        public string? PreferredContactEmail { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        [CustomValidation(typeof(MemberInfo), nameof(ValidateBirthDate))]
         public DateTime? BirthDate { get; set; }
 
         public string? Sex { get; set; }
@@ -46,10 +57,27 @@ namespace TestUserLogIn.Models
 
         public string ApplicationUserID { get; set; } = string.Empty;
 
+        [Display(Name = "Preffered Contact Method")]
+        public bool PrefersPhone { get; set; }
+        public bool PrefersEmail { get; set; }
+        public bool PrefersText { get; set; }
+
         public ApplicationUser? ApplicationUser { get; set; }
         public ICollection<MemberInterest>? MemberInterests { get; set; }
         public ICollection<MemberInvolvement>? MemberInvolvements { get; set; }
         public ICollection<MemberServiceRole>? MemberServiceRoles { get; set; }
 
+        // Custom validation method for BirthDate
+        public static ValidationResult? ValidateBirthDate(DateTime? birthDate, ValidationContext context)
+        {
+            if (birthDate.HasValue)
+            {
+                if (birthDate.Value > DateTime.UtcNow)
+                {
+                    return new ValidationResult("Birth Date cannot be in the future.");
+                }
+            }
+            return ValidationResult.Success;
+        }
     }
 }
